ecc39d0f280f3f8edb9cd8653a5ebeb88ad69331
ninza <ninza147258@gmail.com>
2022-07-10 10:45:49 +0300

    in 'String formatinf' TODO add

--- src/FString.java
+++ src/FString.java
@@ -3,5 +3,6 @@ public class FString {
         System.out.printf("Hello %10.2f world %n", 45.345); // работает - в float можно настраивать сразу до точки и после
         System.out.printf("Check %2d \n", 1000000); // все равно работает, он просто добавляет хвост - доп числа
         System.out.printf("Check %-2d \n", 1000000); // \n - тоже работает как и %n
+        // TODO: узнать зачем тогда ф-ция format, если можно soutf...
     }
 }
