b88c5c10f82f443358c5330ec8cd03654dfb3f6d
ninza <ninza147258@gmail.com>
2022-07-10 10:43:46 +0300

    about 'String formating' add

---
+++ src/FString.java
@@ -0,0 +1,7 @@
+public class FString {
+    public static void main(String[] args) {
+        System.out.printf("Hello %10.2f world %n", 45.345); // работает - в float можно настраивать сразу до точки и после
+        System.out.printf("Check %2d \n", 1000000); // все равно работает, он просто добавляет хвост - доп числа
+        System.out.printf("Check %-2d \n", 1000000); // \n - тоже работает как и %n
+    }
+}
